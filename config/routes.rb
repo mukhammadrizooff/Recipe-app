ad92a0f1e98bdcf8a0554ff29764520b56bfe84f
mukhammadrizooff <legend.it@bk.ru>
2022-06-04 22:56:44 +0500

    Implement devise nad routes for n+1 queries

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,21 @@ Rails.application.routes.draw do
 
   root "public_recipes#index"
 
-  resources :foods, only: [:index, :create, :new, :destroy]
+  resources :public_recipes, only: [:index]
 
   resources :recipes, only: [:index]
   resources :users do 
-    resources :recipes, except: [:update, :edit]
-    resources :foods, only: [:destroy]
+    resources :recipes, except: [:update, :edit] do
+      resources :recipe_foods, only: [:new]
+      resources :shopping_list, only: [:index]
+      
+      resources :foods, only: [:index] do
+        resources :recipe_foods, only: [ :create, :destroy]
+      end
+    end
+    resources :foods, only: [:index, :create, :new, :destroy]
+
   end
 
-  resources :public_recipes, only: [:index]
-  resources :shopping_list, only: [:index]
+
 end
